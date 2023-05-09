bbf7dea5a394e1845f4a830094a1eff04f631a30
shipherman <aleksandr.tyagni@gmail.com>
2023-05-09 16:19:52 +0300

    Fix metrics processing
    - remove go rutine
    - remove some comments
    + add tickers to handle timeouts
    + change MemStorage parameters assigment to Update() function

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -9,12 +9,12 @@ import (
     "time"
     "math/rand"
     "strconv"
+    "strings"
 
     "net/http"
     "net/url"
 
     s "github.com/shipherman/go-metrics/internal/storage"
-//     e "github.com/caarlos0/env/v6"
 
     "os"
 )
@@ -37,13 +37,10 @@ var stat runtime.MemStats
 //server parameters
 var contentType = url.Values{"Content-type": {"text/plain"}}
 
-
-
-
 var logger *log.Logger
 
 
-func parseOptions () {
+func parseOptions () error {
 // 	fmt.Println(options)
     flag.IntVar(&options.pollInterval, "p", 2,
                      "Frequensy in seconds for collecting metrics")
@@ -52,12 +49,6 @@ func parseOptions () {
     flag.StringVar(&options.serverAddress, "a", "localhost:8080",
                 "Address of the server to send metrics")
     flag.Parse()
-// 	fmt.Println(options)
-
-    // env/v6 не работает )Ж
-//     if err := e.Parse(&options); err != nil {
-//         fmt.Println(err)
-//     }
 
     if l := os.Getenv("ADDRESS"); l != "" {
         options.serverAddress = l
@@ -65,19 +56,18 @@ func parseOptions () {
     if l := os.Getenv("POLL_INTERVAL"); l != "" {
         i, err := strconv.Atoi(l)
         if err != nil {
-            panic(err)
+            return err
         }
         options.pollInterval = i
     }
     if l := os.Getenv("REPORT_INTERVAL"); l != "" {
         i, err := strconv.Atoi(l)
         if err != nil {
-            panic(err)
+            return err
         }
         options.reportInterval = i
     }
-
-// 	fmt.Println(options, os.Getenv("POLL_INTERVAL"), os.Getenv("REPORT_INTERVAL") )
+    return nil
 }
 
 func SendPostRequest (req string) error {
@@ -85,7 +75,6 @@ func SendPostRequest (req string) error {
     if err != nil {
         return err
     }
-    //     fmt.Println(resp.Status)
     if resp.StatusCode != http.StatusOK {
         line, err := io.ReadAll(resp.Body)
         if err != nil {
@@ -100,7 +89,7 @@ func SendPostRequest (req string) error {
     return nil
 }
 
-func ProcessReport (data s.MemStorage) error {
+func ProcessReport (data *s.MemStorage) error {
     // metric type variable
     var mtype string
 
@@ -112,7 +101,11 @@ func ProcessReport (data s.MemStorage) error {
             case s.Counter:
                 mtype = "counter"
         }
-        req := "http://" + options.serverAddress + "/update/" + mtype + fmt.Sprintf("/%v/%v",k,v)
+        req := strings.Join([]string{"http:/",
+                         options.serverAddress,
+                         "update",
+                         mtype,
+                         fmt.Sprintf("%v/%v",k,v)}, "/")
         err := SendPostRequest(req)
         if err != nil {
             return err
@@ -121,61 +114,66 @@ func ProcessReport (data s.MemStorage) error {
     return nil
 }
 
+func readMemStats(m *s.MemStorage) {
+    runtime.ReadMemStats(&stat)
+    m.Update("gauge", "Alloc", s.Gauge(stat.Alloc))
+    m.Update("gauge", "BuckHashSys", s.Gauge(stat.BuckHashSys))
+    m.Update("gauge", "Frees", s.Gauge(stat.Frees))
+    m.Update("gauge", "GCCPUFraction", s.Gauge(stat.GCCPUFraction))
+    m.Update("gauge", "GCSys", s.Gauge(stat.GCSys))
+    m.Update("gauge", "HeapAlloc", s.Gauge(stat.HeapAlloc))
+    m.Update("gauge", "HeapIdle", s.Gauge(stat.HeapIdle))
+    m.Update("gauge", "HeapInuse", s.Gauge(stat.HeapInuse))
+    m.Update("gauge", "HeapObjects", s.Gauge(stat.HeapObjects))
+    m.Update("gauge", "HeapReleased", s.Gauge(stat.HeapReleased))
+    m.Update("gauge", "HeapSys", s.Gauge(stat.HeapSys))
+    m.Update("gauge", "LastGC", s.Gauge(stat.LastGC))
+    m.Update("gauge", "Lookups", s.Gauge(stat.Lookups))
+    m.Update("gauge", "MCacheInuse", s.Gauge(stat.MCacheInuse))
+    m.Update("gauge", "MCacheSys", s.Gauge(stat.MCacheSys))
+    m.Update("gauge", "MSpanInuse", s.Gauge(stat.MSpanInuse))
+    m.Update("gauge", "MSpanSys", s.Gauge(stat.MSpanSys))
+    m.Update("gauge", "Mallocs", s.Gauge(stat.Mallocs))
+    m.Update("gauge", "NextGC", s.Gauge(stat.NextGC))
+    m.Update("gauge", "NumForcedGC", s.Gauge(stat.NumForcedGC))
+    m.Update("gauge", "NumGC", s.Gauge(stat.NumGC))
+    m.Update("gauge", "OtherSys", s.Gauge(stat.OtherSys))
+    m.Update("gauge", "PauseTotalNs", s.Gauge(stat.PauseTotalNs))
+    m.Update("gauge", "StackInuse", s.Gauge(stat.StackInuse))
+    m.Update("gauge", "StackSys", s.Gauge(stat.StackSys))
+    m.Update("gauge", "Sys", s.Gauge(stat.Sys))
+    m.Update("gauge", "TotalAlloc", s.Gauge(stat.TotalAlloc))
+    m.Update("gauge", "RandomValue", s.Gauge(rand.Float32()))
+    m.Update("counter", "PollCount", s.Counter(1))
+}
+
 func main() {
     //parse cli options
-    parseOptions()
+    err := parseOptions()
+    if err != nil {
+        panic(err)
+    }
 
     // initiate conters
     m.Data["PollCount"] = s.Counter(0)
 
-    go func() {
-        for {
-            //collect data from MemStats
-            runtime.ReadMemStats(&stat)
-            m.Data["Alloc"] = s.Gauge(stat.Alloc)
-            m.Data["BuckHashSys"] = s.Gauge(stat.BuckHashSys)
-            m.Data["Frees"] = s.Gauge(stat.Frees)
-            m.Data["GCCPUFraction"] = s.Gauge(stat.GCCPUFraction)
-            m.Data["GCSys"] = s.Gauge(stat.GCSys)
-            m.Data["HeapAlloc"] = s.Gauge(stat.HeapAlloc)
-            m.Data["HeapIdle"] = s.Gauge(stat.HeapIdle)
-            m.Data["HeapInuse"] = s.Gauge(stat.HeapInuse)
-            m.Data["HeapObjects"] = s.Gauge(stat.HeapObjects)
-            m.Data["HeapReleased"] = s.Gauge(stat.HeapReleased)
-            m.Data["HeapSys"] = s.Gauge(stat.HeapSys)
-            m.Data["LastGC"] = s.Gauge(stat.LastGC)
-            m.Data["Lookups"] = s.Gauge(stat.Lookups)
-            m.Data["MCacheInuse"] = s.Gauge(stat.MCacheInuse)
-            m.Data["MCacheSys"] = s.Gauge(stat.MCacheSys)
-            m.Data["MSpanInuse"] = s.Gauge(stat.MSpanInuse)
-            m.Data["MSpanSys"] = s.Gauge(stat.MSpanSys)
-            m.Data["Mallocs"] = s.Gauge(stat.Mallocs)
-            m.Data["NextGC"] = s.Gauge(stat.NextGC)
-            m.Data["NumForcedGC"] = s.Gauge(stat.NumForcedGC)
-            m.Data["NumGC"] = s.Gauge(stat.NumGC)
-            m.Data["OtherSys"] = s.Gauge(stat.OtherSys)
-            m.Data["PauseTotalNs"] = s.Gauge(stat.PauseTotalNs)
-            m.Data["StackInuse"] = s.Gauge(stat.StackInuse)
-            m.Data["StackSys"] = s.Gauge(stat.StackSys)
-            m.Data["Sys"] = s.Gauge(stat.Sys)
-            m.Data["TotalAlloc"] = s.Gauge(stat.TotalAlloc)
-            m.Data["RandomValue"] = s.Gauge(rand.Float32())
-            m.Data["PollCount"] = m.Data["PollCount"].(s.Counter) + 1
+    // initiate tickers
+    pollTicker := time.NewTicker(time.Second * time.Duration(options.pollInterval))
+	defer pollTicker.Stop()
+    reportTicker := time.NewTicker(time.Second * time.Duration(options.reportInterval))
+	defer reportTicker.Stop()
 
-            //collect timeout
-            time.Sleep(time.Duration(options.pollInterval) * time.Second)
-
-            }
-        }()
-
-    //send collected data to the server
+    //collect data from MemStats and send to the server
     for {
-        err := ProcessReport(m)
-        if err != nil {
-            log.Println(err)
+        select {
+        case <-pollTicker.C:
+            readMemStats(&m)
+        case <-reportTicker.C:
+            //send collected data to the server
+            err := ProcessReport(&m)
+            if err != nil {
+                log.Println(err)
+            }
         }
-        //report timeout
-        time.Sleep(time.Duration(options.reportInterval) * time.Second)
-
     }
 }
