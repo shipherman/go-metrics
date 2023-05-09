d77571e8781f893aa0b622ddbed66616acb4660f
shipherman <aleksandr.tyagni@gmail.com>
2023-05-09 13:45:26 +0300

    Separate hadlers from main package

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "io"
     "os"
-    "fmt"
+//     "fmt"
     "flag"
     "net/http"
     "log"
-    "strconv"
 
     "github.com/go-chi/chi/v5"
+
     s "github.com/shipherman/go-metrics/internal/storage"
+    h "github.com/shipherman/go-metrics/internal/handlers"
 )
 
 //storage
@@ -23,68 +23,6 @@ var options struct {
     address string
 }
 
-
-func HandleMain(w http.ResponseWriter, r *http.Request) {
-    //write static html page with all the items to the response; unsorted
-    body := `
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>All tuples</title>
-    </head>
-    <body>
-      <table>
-          <tr>
-            <td>Metric</td>
-            <td>Value</td>
-          </tr>
-    `
-    list := mem.GetAll()
-    for k, v := range list {
-        body = body + fmt.Sprintf("<tr>\n<td>%s</td>\n", k)
-        body = body + fmt.Sprintf("<td>%v</td>\n</tr>\n", v)
-    }
-    body = body + " </table>\n </body>\n</html>"
-
-    w.Write([]byte(body))
-}
-
-func HandleUpdate (w http.ResponseWriter, r *http.Request) {
-    //http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>,
-    //Content-Type: text/plain
-
-    // get context params
-    metricType := chi.URLParam(r, "type")
-    metric := chi.URLParam(r, "metric")
-    value := chi.URLParam(r, "value")
-
-    // find out metric type
-    switch metricType {
-        case "counter":
-            v, err := strconv.Atoi(value)
-            if err != nil {
-                http.Error(w, err.Error(), http.StatusBadRequest)
-            }
-            mem.Update(metricType, metric, s.Counter(v))
-        case "gauge":
-            v, err := strconv.ParseFloat(value, 64)
-            if err != nil {
-                http.Error(w, err.Error(), http.StatusBadRequest)
-            }
-            mem.Update(metricType, metric, s.Gauge(v))
-        default:
-            http.Error(w, "Incorrect metric type", http.StatusBadRequest)
-    }
-}
-
-func HandleValue (w http.ResponseWriter, r *http.Request) {
-    metric := chi.URLParam(r, "metric")
-    v, err := mem.Get(metric)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusNotFound)
-    }
-    io.WriteString(w, fmt.Sprintf("%v", v))
-}
 
 func main() {
     //parse cli options
@@ -100,10 +38,10 @@ func main() {
 
     // Routers
     router := chi.NewRouter()
-    router.Get("/", HandleMain)
-    router.Post("/update/{type}/{metric}/{value}", HandleUpdate)
-    router.Get("/value/gauge/{metric}", HandleValue)
-    router.Get("/value/counter/{metric}", HandleValue)
+    router.Get("/", h.HandleMain(&mem))
+    router.Post("/update/{type}/{metric}/{value}", h.HandleUpdate(&mem))
+    router.Get("/value/gauge/{metric}", h.HandleValue(&mem))
+    router.Get("/value/counter/{metric}", h.HandleValue(&mem))
 
     log.Println("Starting server...")
     //run server
