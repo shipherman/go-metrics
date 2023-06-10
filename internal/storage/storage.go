4b557d4a684cffe5f75adc5a0a15d043fd9a3142
shipherman <aleksandr.tyagni@gmail.com>
2023-06-10 23:52:31 +0300

    Change storage saving to file
    + move file read/write functions to separate file
    + add StorageWriter interface
    + implement StorageWriter for LocalFile (json)

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,9 +1,8 @@
+// Implements storing data in RAM
 package storage
 
 import (
     "fmt"
-    "os"
-    "encoding/json"
 )
 
 type Counter int64
@@ -14,6 +13,21 @@ type MemStorage struct {
     GaugeData map[string]Gauge
 }
 
+// Define methods to write/read data from different providers
+type StorageWriter interface {
+    Write (s MemStorage) error
+    RestoreData (s *MemStorage) error
+//     Save () error
+}
+
+// Write data to store
+func SaveData(m MemStorage, sw StorageWriter) error {
+    err := sw.Write(m)
+    if err != nil {
+            return err
+    }
+    return nil
+}
 
 func New() (MemStorage) {
     return MemStorage{
@@ -47,44 +61,4 @@ func (m *MemStorage) UpdateGauge(metric string, value Gauge) {
 
 func (m *MemStorage) UpdateCounter(metric string, value Counter) {
     m.CounterData[metric] = m.CounterData[metric] + value
-}
-
-func cleanFile(filename string) error {
-    f, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE, 0666)
-    if err != nil {
-        return err
-    }
-    defer f.Close()
-
-
-    err = f.Truncate(0)
-    if err != nil {
-        return err
-            }
-    return nil
-}
-
-func WriteDataToFile(filename string, store MemStorage) error {
-    //clear file
-    err := cleanFile(filename)
-    if err != nil {
-        return err
-    }
-
-    f, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE, 0666)
-    if err != nil {
-        return err
-    }
-    defer f.Close()
-
-    data, err := json.MarshalIndent(store, "", "  ")
-    if err != nil {
-        return err
-    }
-
-    _, err = f.Write(data)
-    if err != nil {
-        return err
-    }
-    return nil
 }
