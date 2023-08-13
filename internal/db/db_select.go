257bd139f0985d1c2fa9f6b839bf4c87fc18630f
shipherman <aleksandr.tyagni@gmail.com>
2023-08-13 23:23:29 +0300

    Fix Select All db request

--- internal/db/db_select.go
+++ internal/db/db_select.go
@@ -1,30 +1,27 @@
 package db
 
 import (
-    "fmt"
-    "context"
-
+	"context"
+	"fmt"
 )
 
-
 func (db *Database) SelectAll() error {
-    rows, err := db.Conn.Query(context.Background(),
-                           `SELECT * FROM counter_metrics
-                           UNION
-                           SELECT * FROM gauge_metrics
-                           Limit 10`)
-    if err != nil {
-            return err
-    }
+	rows, err := db.Conn.Query(context.Background(),
+		`SELECT * FROM counter_metrics
+        UNION
+        SELECT * FROM gauge_metrics`)
+	if err != nil {
+		return err
+	}
 
-    fmt.Println("SelectAll")
-    for rows.Next() {
-        data, err := rows.Values()
-        if err != nil {
-                return err
-        }
-        fmt.Println(data)
-    }
+	fmt.Println("SelectAll")
+	for rows.Next() {
+		data, err := rows.Values()
+		if err != nil {
+			return err
+		}
+		fmt.Println(data)
+	}
 
-    return nil
+	return nil
 }
