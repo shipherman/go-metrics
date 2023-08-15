3510734c4e0c4fa950e899fafcefb70db2df0050
shipherman <aleksandr.tyagni@gmail.com>
2023-08-15 17:36:23 +0300

    Fix race condition for storage interface

--- internal/db/db_restore.go
+++ internal/db/db_restore.go
@@ -1,14 +1,13 @@
 package db
 
 import (
-    "github.com/shipherman/go-metrics/internal/storage"
-
+	"github.com/shipherman/go-metrics/internal/storage"
 )
 
-//Implements StorageWriter interface
-func (db *Database) RestoreData(s *storage.MemStorage) error {
-    //to do
+// Implements StorageWriter interface
+func (db *Database) RestoreData(s storage.MemStorage) error {
+	//to do
 
-    return nil
+	return nil
 
 }
