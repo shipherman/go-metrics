aa60274b275597cb6da229f5a6eb1735c686e48e
shipherman <aleksandr.tyagni@gmail.com>
2023-09-18 01:42:43 +0300

    Add -c cli option to provide agent configuration file

--- cmd/agent/options.go
+++ cmd/agent/options.go
@@ -2,29 +2,41 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"os"
 	"strings"
 
 	"github.com/caarlos0/env"
 )
 
 type Options struct {
-	ServerAddress    string `env:"ADDRESS"`
-	PollInterval     int    `env:"POLL_INTERVAL"`
-	MaxRetryInterval int
-	ReportInterval   int    `env:"REPORT_INTERVAL"`
-	RateLimit        int    `env:"RATE_LIMIT"`
-	Key              string `env:"KEY"`
-	CryptoKey        string `env:"CRYPTO_KEY"`
+	ServerAddress    string `env:"ADDRESS" json:"server_address"`
+	PollInterval     int    `env:"POLL_INTERVAL" json:"poll_interval"`
+	MaxRetryInterval int    `env:"MAX_RETRY_INTERVAL" json:"max_retry_interval"`
+	ReportInterval   int    `env:"REPORT_INTERVAL" json:"report_interval"`
+	RateLimit        int    `env:"RATE_LIMIT" json:"rate_limit"`
+	Key              string `env:"KEY" json:"key"`
+	CryptoKey        string `env:"CRYPTO_KEY" json:"crypto_key"`
 	KeyByte          []byte
-
-	Encrypt bool
+	ConfigPath       string `env:"CONFIG"`
+	Encrypt          bool
 }
 
 func parseOptions() (Options, error) {
 	var cfg Options
 	cfg.Encrypt = false
 
+	// Parse environment variables
+	err := env.Parse(&cfg)
+	if err != nil {
+		return cfg, err
+	}
+
+	// Parse cli parameters
+	flag.StringVar(&cfg.ConfigPath,
+		"config", "",
+		"Configuration file path")
 	flag.IntVar(&cfg.PollInterval, "p", 2,
 		"Frequensy in seconds for collecting metrics")
 	flag.IntVar(&cfg.MaxRetryInterval, "m", 4,
@@ -41,6 +53,15 @@ func parseOptions() (Options, error) {
 		"Rate Limit")
 	flag.Parse()
 
+	// Read configuration file
+	if cfg.ConfigPath != "" {
+		cfg, err = readConfigFile(cfg.ConfigPath)
+		if err != nil {
+			return cfg, err
+		}
+	}
+
+	// Save endpoint address
 	cfg.ServerAddress = strings.Join([]string{"http:/", cfg.ServerAddress, "updates/"}, "/")
 
 	if cfg.Key != "" {
@@ -48,7 +69,16 @@ func parseOptions() (Options, error) {
 		cfg.KeyByte = []byte(cfg.Key)
 	}
 
-	err := env.Parse(&cfg)
+	return cfg, nil
+}
+
+func readConfigFile(path string) (cfg Options, err error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return cfg, err
+	}
+
+	err = json.Unmarshal(data, &cfg)
 	if err != nil {
 		return cfg, err
 	}
